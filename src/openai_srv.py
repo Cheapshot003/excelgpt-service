0d51858e2320248fff40150c978384b62893ce96
cheapshot003 <dercheapi@protonmail.com>
2024-01-28 19:26:31 +0100

    auth1

--- src/openai_srv.py
+++ src/openai_srv.py
@@ -14,7 +14,7 @@ def send_prompt(prompt: Prompt):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are my assistant at my work at KPMG. Act accordingly. If you get a question or assignment, just do it and return the raw answer without any sentences before it or something. I want to integrate you into excel, so return the data in a format that would fit into an excel cell. Also be prepared to provide things like excel formulas. Thanks!"},
+            {"role": "system", "content": "You are my assistant at my work at KPMG. Act accordingly. If you get a question or assignment, just do it and return the raw answer without any sentences before it or something. I want to integrate you into excel, so return the data in a format that would fit into an excel cell. Thanks!"},
             {"role": "user", "content": str(prompt.prompt)},
             {"role": "user", "content": str(prompt.data)}
     ]
